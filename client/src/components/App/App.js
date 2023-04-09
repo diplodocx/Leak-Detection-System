6874a9149587572a40eb3123dea9823141e6aed5
OreoBlunt1 <dima_zhdanov_02@mail.ru>
2023-04-09 23:59:04 +0300

    added home page

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../../pages/Login/Login';
-import SignIn from '../../pages/Example';
 import Dick from '../Dick/Dick';
+import Home from '../../pages/Home/Home';
 
 export default function App() {
 	return (
 		<Routes>
 			<Route path='/login' element={<Login />} />
-			<Route path='/signin' element={<SignIn />} />
 			<Route path='/dick' element={<Dick />} />
+			<Route path='/home' element={<Home />} />
 			<Route path='/' element={<Navigate to='/login' />} />
 		</Routes>
 	);
---
+++ client/src/components/LeakList/LeakList.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import classes from './LeakList.module.css';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+
+function LeakList({ page }) {
+	const [leaks, setLeaks] = useState([]);
+
+	useEffect(() => {
+		axios.get('/leaks').then((res) => {
+			const leakData = res.data.leak_data;
+			const newLeaks = leakData.map((leak, index) => {
+				return { id: leak._id, mac: leak.mac, time: leak.time };
+			});
+
+			setLeaks(newLeaks);
+		});
+	}, []);
+
+	const columns = [
+		{ field: 'id', headerName: 'ID', width: 90 },
+		{ field: 'mac', headerName: 'Мак адрес', width: 150 },
+		{ field: 'time', headerName: 'Время', width: 250 },
+	];
+
+	return (
+		<div className={classes.LeakList}>
+			<DataGrid
+				sx={{
+					borderRadius: 0,
+				}}
+				rows={leaks}
+				columns={columns}
+				autoPageSize
+			/>
+		</div>
+	);
+}
+
+export default LeakList;
